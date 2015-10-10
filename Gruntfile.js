8f4319a5fc0cced82e5d96241be8f1698ca92364
Paladox <thomasmulhall410@yahoo.com>
2015-10-11 00:18:59 +0100

    Update jenkins tests
    Add support for running composer test (php linting) and npm test
    (jsonlint and jshint).
    
    Change-Id: Iae0d280f8c10bd71933f59db39bf9f027ddbebaa

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'**/*.js'
+			]
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'jshint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
