49c03befa798cdcfa5f37375897b51c3baf88d74
Umherirrender <umherirrender_de.wp@web.de>
2017-10-21 18:59:20 +0200

    Add missing qqq message documentation
    Activate banana checker
    
    Bug: T155437
    Change-Id: I6adafe44f838bff9f3e50e19cd5eb7c2ebbf1888

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
 		jshint: {
@@ -12,6 +13,9 @@ module.exports = function ( grunt ) {
 				'**/*.js'
 			]
 		},
+		banana: {
+			all: 'i18n/'
+		},
 		jsonlint: {
 			all: [
 				'**/*.json',
@@ -21,6 +25,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'jshint' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'jshint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
