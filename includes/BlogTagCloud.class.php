741b36c93e8843235048a252f122c24c050820d2
Kunal Mehta <legoktm@member.fsf.org>
2018-05-23 22:09:43 -0700

    Autofixing MediaWiki.Commenting.LicenseComment.InvalidLicenseTag errors
    Change-Id: I1b604df817c9972fc98c65f90dada2a7f2e6fad3

--- includes/BlogTagCloud.class.php
+++ includes/BlogTagCloud.class.php
@@ -2,7 +2,7 @@
 /**
  * @file
  * @copyright Copyright © 2007, Wikia Inc.
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license GPL-2.0-or-later
  */
 class BlogTagCloud {
 	/** @var integer $tags_min_pts **/
