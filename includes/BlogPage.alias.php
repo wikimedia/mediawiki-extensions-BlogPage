4530174bc4500f532ec7df84e3f40b58978a7a74
anterdc99 <xuminruixuan@live.cn>
2025-07-13 15:17:41 +0800

    BlogPage: Translate special page aliases into Chinese
    Change-Id: Ie58584f08a389b5688a162d63db11be08227249b

--- includes/BlogPage.alias.php
+++ includes/BlogPage.alias.php
@@ -21,3 +21,29 @@ $specialPageAliases['fi'] = [
 	'ArticleLists' => [ 'Artikkelilistat' ],
 	'CreateBlogPost' => [ 'Luo blogikirjoitus' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'ArticlesHome' => [ 'ArticlesHome' ],
+	'ArticleLists' => [ 'ArticleLists' ],
+	'CreateBlogPost' => [ 'CreateBlogPost' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'ArticlesHome' => [ '条目首页' ],
+	'ArticleLists' => [ '条目列表' ],
+	'CreateBlogPost' => [ '创建博客文章' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'ArticlesHome' => [ '條目首頁' ],
+	'ArticleLists' => [ '條目列表' ],
+	'CreateBlogPost' => [ '建立部落格文章' ],
+];
+
+/** Chinese (Hong Kong) (中文（香港）) */
+$specialPageAliases['zh-hk'] = [
+	'CreateBlogPost' => [ '建立網誌文章' ],
+];
